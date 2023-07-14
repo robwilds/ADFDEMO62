fa4e38a58079b3996e125dbc0b2d51a8a5354e89
Rob Wilds <Robert.wilds@hyland.com>
2023-07-14 17:25:51 -0400

    removed contracts module

--- demo-shell/src/app/contracts/contracts/contracts.component.ts
+++ demo-shell/src/app/contracts/contracts/contracts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchFilterChipsComponent } from '../../components/search/search-filter-chips.component';
+import { SearchBarComponent } from '../../components/search/search-bar.component';
 
 @Component({
   selector: 'contracts',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContractsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private SearchBarComponent:SearchBarComponent) {
+()
+    SearchBarComponent.onSearchSubmit()
+   }
 
   ngOnInit(): void {
   }
